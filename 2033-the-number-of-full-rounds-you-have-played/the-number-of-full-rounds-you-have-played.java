d57c7d0fe082dfdae687f2e7c97cdac942b2debc
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2024-10-15 17:12:00 +0530

    Time: 3 ms (11.11%) | Memory: 41.2 MB (82.05%) - LeetSync

---
+++ 2033-the-number-of-full-rounds-you-have-played/the-number-of-full-rounds-you-have-played.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int convertToMinutes(String a, String b) {
+        int num1 = (a.charAt(0) - '0') * 10 + a.charAt(1) - '0';
+        int num2 = (b.charAt(0) - '0') * 10 + b.charAt(1) - '0';
+        System.out.println(num1);
+        return num1 * 60 + num2;
+    }
+
+    public int numberOfRounds(String loginTime, String logoutTime) {
+        // 9*60+31=571/15;
+        // 10*60+14=614/15;
+        // 1> 38 rem=1
+        // 2> 40
+        // int a = String.atoi()[0]);
+        String arr1[] = loginTime.split(":");
+        String arr2[] = logoutTime.split(":");
+        int a = convertToMinutes(arr1[0], arr1[1]);
+        int b = convertToMinutes(arr2[0], arr2[1]);
+        if (b < a) {
+            b = b + (24 * 60);
+        }
+        int diva = a / 15;
+        int rem = a % 15;
+        int divb = b / 15;
+        int ans = rem == 0 ? divb - diva : divb - diva - 1;
+        return ans == -1 ? 0 : ans;
+    }
+}
