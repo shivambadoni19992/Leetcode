12604b9d1bcb3caeb461b4dc7a87ae706e34b64a
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2024-12-06 14:29:31 +0530

    Time: 51 ms (31.52%) | Memory: 45.9 MB (33.18%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -1,7 +1,6 @@
 class Solution {
 
     public int maxCount(int[] banned, int n, int maxSum) {
-        Arrays.sort(banned);
         Map<Integer,Integer> map= new HashMap<>();
         for(int i=0;i<banned.length;i++){
             map.put(banned[i],i);
