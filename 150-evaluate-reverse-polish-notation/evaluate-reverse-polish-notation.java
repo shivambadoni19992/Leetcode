d3a6ce6fe0d7496c962c92c0e907859af2a794cb
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2024-12-24 17:49:24 +0530

    Time: 7 ms (31.87%) | Memory: 45.1 MB (15.48%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int convert(String a){
+        int num=1,i=0;
+        if(a.charAt(0)=='-') {
+            num=-1;
+            i++;
+        }
+        int sum=0;
+        while(i<a.length()){
+            sum = sum*10 + (a.charAt(i)-'0');
+            i++;
+        }
+        return sum * num;
+
+    }
+    public int evalRPN(String[] tokens) {
+        Stack <Integer> stack = new Stack();
+        for(int i=0;i<tokens.length;i++){
+            String ch = "";
+            ch += tokens[i];
+            int a,b,sum;
+            switch(ch){
+                case "+":
+                    a = stack.pop();
+                    b = stack.pop();
+                    sum = b + a;
+                    stack.push(sum);
+                    break;
+                case "-":
+                    a = stack.pop();
+                    b = stack.pop();
+                    sum = b - a;
+                    stack.push(sum);
+                    break;
+                case "*":
+                    a = stack.pop();
+                    b = stack.pop();
+                    sum = b * a;
+                    stack.push(sum);
+                    break;
+                case "/":
+                    a = stack.pop();
+                    b = stack.pop();
+                    sum = b / a;
+                    stack.push(sum);
+                    break;
+                default:
+                    stack.push(convert(ch));
+            }
+        }
+        return stack.pop();
+    }
+}
