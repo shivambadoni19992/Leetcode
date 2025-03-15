b4923f8c5338134ef18fbcd2666058f6c8aa2af3
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-03-15 14:50:55 +0530

    Time: 20 ms (40.30%) | Memory: 65.2 MB (14.43%) - LeetSync

---
+++ 2690-house-robber-iv/house-robber-iv.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public int minCapability(int[] nums, int k) {
+        // Store the maximum nums value in maxReward.
+        int minReward = 1;
+        int maxReward = Arrays.stream(nums).max().getAsInt();
+        int totalHouses = nums.length;
+
+        // Use binary search to find the minimum reward possible.
+        while (minReward < maxReward) {
+            int midReward = (minReward + maxReward) / 2;
+            int possibleThefts = 0;
+
+            for (int index = 0; index < totalHouses; ++index) {
+                if (nums[index] <= midReward) {
+                    possibleThefts += 1;
+                    index++;
+                }
+            }
+
+            if (possibleThefts >= k) maxReward = midReward;
+            else minReward = midReward + 1;
+        }
+
+        return minReward;
+    }
+}
