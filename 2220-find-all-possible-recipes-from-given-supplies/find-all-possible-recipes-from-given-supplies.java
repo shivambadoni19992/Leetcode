30f1873d301f0dd208db65143d74082de2683f79
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-03-21 16:05:00 +0530

    Time: 59 ms (58.28%) | Memory: 46.6 MB (79.14%) - LeetSync

---
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        List<String> suppliesList = new ArrayList<>(Arrays.asList(supplies));
+        List<String> output = new ArrayList<>();
+        Map<String, Integer> inDegree = new HashMap<>();
+        Map<String, List<String>> adjacenyList = new HashMap<>();
+        for (int i = 0; i < ingredients.size(); i++) {
+            String recipe = recipes[i];
+            int items = ingredients.get(i).size();
+            inDegree.put(recipe, items);
+            for (int j = 0; j < items; j++) {
+                String key = ingredients.get(i).get(j);
+                if (!adjacenyList.containsKey(key)) {
+                    adjacenyList.put(key, new ArrayList<>());
+                }
+                adjacenyList.get(key).add(recipe);
+            }
+        }
+        for (int i = 0; i < suppliesList.size(); i++) {
+            String supply = suppliesList.get(i);
+            if (adjacenyList.containsKey(supply)) {
+                List<String> recipe = adjacenyList.get(supply);
+                for (int j = 0; j < recipe.size(); j++) {
+                    String ele = recipe.get(j);
+                    int inDeg = inDegree.get(ele) - 1;
+                    inDegree.put(ele, inDeg);
+                    if (inDeg == 0) {
+                        output.add(ele);
+                        suppliesList.add(ele);
+                    }
+                }
+            }
+        }
+
+        return output;
+    }
+}
