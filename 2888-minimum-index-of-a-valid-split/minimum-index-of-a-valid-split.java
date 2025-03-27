09ae43e0c88bfb0fdd96e903543d3c8a866ad548
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-03-27 15:59:10 +0530

    Time: 544 ms (5.13%) | Memory: 56.5 MB (86.54%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int minimumIndex(List<Integer> nums) {
+        int count = 0, n = nums.size();
+        int majorityElement = -1;
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                majorityElement = nums.get(i);
+                count = 1;
+            } else if (nums.get(i) == majorityElement) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        int totalCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (majorityElement == nums.get(i)) {
+                totalCount++;
+            }
+        }
+        if (totalCount <= n / 2) return -1;
+        int leftCount = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) == majorityElement) {
+                leftCount++;
+                totalCount--;
+            }
+            System.out.println(totalCount + " " + leftCount);
+            if (((leftCount * 2) > (i + 1)) && (totalCount * 2) > (n - i - 1)) return i;
+        }
+        return -1;
+    }
+}
