a45f0e4a90d13fbc54c93f30ddf67f5a5652ae23
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-04-06 22:26:56 +0530

    Time: 37 ms (28.20%) | Memory: 55.5 MB (18.43%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,6 +1,7 @@
 class Pair {
     double dist;
-    int []arr;
+    int[] arr;
+
     public Pair(double dist, int[] arr) {
         this.dist = dist;
         this.arr = arr;
@@ -8,24 +9,28 @@ class Pair {
 }
 
 class Solution {
+
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(new Comparator<Pair>(){
-            @Override
-            public int compare(Pair a ,Pair b){
-                return Double.compare(b.dist, a.dist);
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+            new Comparator<Pair>() {
+
+                @Override
+                public int compare(Pair a, Pair b) {
+                    return Double.compare(b.dist, a.dist);
+                }
             }
-        });
-        for(int point[] : points){
-            double dist = Math.sqrt((point[0]*point[0]) + (point[1] * point[1]));
-            pq.add(new Pair(dist,point));
-            if(pq.size() > k){
+        );
+        for (int point[] : points) {
+            double dist = Math.sqrt((point[0] * point[0]) + (point[1] * point[1]));
+            pq.add(new Pair(dist, point));
+            if (pq.size() > k) {
                 pq.poll();
             }
         }
         int ans[][] = new int[pq.size()][2];
         int i = 0;
-        while(!pq.isEmpty()) {   
-            ans[i++] = pq.poll().arr; 
+        while (!pq.isEmpty()) {
+            ans[i++] = pq.poll().arr;
         }
         return ans;
     }
