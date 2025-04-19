d413aa746426f6ddb0c4038734b94fe00cf2e36e
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-04-19 10:31:41 +0530

    Time: 1043 ms (5.01%) | Memory: 130.7 MB (5.48%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,70 +1,37 @@
-class Node {
-    int key;
-    int val;
-    Node prev;
-    Node next;
-
-    Node(int key, int val) {
-        this.key = key;
-        this.val = val;
-    }
-}
-
 class LRUCache {
-    Node head = new Node(-1, -1);
-    Node tail = new Node(-1, -1);
     int cap;
-    Map<Integer, Node> map = new HashMap<>();
+    Queue<Integer> queue = new LinkedList<>();
+    Map<Integer,Integer> map = new HashMap<>();
 
     public LRUCache(int capacity) {
-        cap = capacity;
-        head.next = tail;
-        tail.prev = head;
+        this.cap = capacity;
     }
-
-    private void addNode(Node newnode) {
-        Node temp = head.next;
-        newnode.next = temp;
-        newnode.prev = head;
-        head.next = newnode;
-        temp.prev = newnode;
-    }
-
-    private void deleteNode(Node delnode) {
-        Node prev = delnode.prev;
-        Node next = delnode.next;
-
-        prev.next = next;
-        next.prev = prev;
-    }
-
+    
     public int get(int key) {
-        if (map.containsKey(key)) {
-            Node res = map.get(key);
-            int ans = res.val;
-            map.remove(key);
-            deleteNode(res);
-            addNode(res);
-            map.put(key, head.next);
-            return ans;
+        if(map.containsKey(key)){
+            queue.remove(key);
+            queue.add(key);
+            return map.get(key);
         }
         return -1;
+        
     }
-
+    
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            Node curr = map.get(key);
-            map.remove(key);
-            deleteNode(curr);
+        if(map.containsKey(key)){
+            queue.remove(key);
+            queue.add(key);
+            map.put(key,value);
+        } else {
+            if(queue.size() == cap){
+                map.remove(queue.poll());
+            }
+            queue.add(key);
+            map.put(key,value);
         }
-        if (map.size() == cap) {
-            map.remove(tail.prev.key);
-            deleteNode(tail.prev);
-        }
-        addNode(new Node(key, value));
-        map.put(key, head.next);
     }
 }
+
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
