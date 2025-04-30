c3b6777771e35774cfe790ed9f3833d2e49d8a74
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-04-30 10:45:38 +0530

    Time: 1 ms (99.95%) | Memory: 43.8 MB (29.54%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,19 +1,21 @@
 class Solution {
     List<List<Integer>> ans;
-    public void find(int start,int []nums,List<Integer> list){
+
+    public void find(int start, int[] nums, List<Integer> list) {
         ans.add(new ArrayList(list));
-        for(int i=start;i<nums.length;i++){
-            if(i!=start&& nums[i]==nums[i-1]) continue;
+        for (int i = start; i < nums.length; i++) {
+            if (i != start && nums[i] == nums[i - 1]) continue;
             list.add(nums[i]);
-            find(i+1,nums,list);
-            list.remove(list.size()-1);
+            find(i + 1, nums, list);
+            list.remove(list.size() - 1);
         }
     }
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        find(0,nums,list);
+        find(0, nums, list);
 
         return ans;
     }
