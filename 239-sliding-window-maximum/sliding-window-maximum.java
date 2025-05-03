1b55ace934166532a6bd0ff4ea42925cbe16599a
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-05-03 15:17:04 +0530

    Time: 42 ms (21.14%) | Memory: 62.1 MB (23.70%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,23 +1,25 @@
 class Solution {
-
     public int[] maxSlidingWindow(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-            (a, b) -> {
-                return nums[b] - nums[a];
+        List<Integer> res = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int idx = 0; idx < nums.length; idx++) {
+            int num = nums[idx];
+
+            while (!deque.isEmpty() && deque.getLast() < num) {
+                deque.pollLast();
             }
-        );
-        int n = nums.length;
-        int[] ans = new int[n - k + 1];
-        int idx = 0;
-        for (int i = 0; i < n; i++) {
-            while (pq.size() > 0 && pq.peek() <= i - k) {
-                pq.remove();
+            deque.addLast(num);
+
+            if (idx >= k && nums[idx - k] == deque.getFirst()) {
+                deque.pollFirst();
             }
-            pq.add(i);
-            if (i >= k - 1) {
-                ans[idx++] = nums[pq.peek()];
+
+            if (idx >= k - 1) {
+                res.add(deque.getFirst());
             }
         }
-        return ans;
+
+        return res.stream().mapToInt(i -> i).toArray();        
     }
 }
