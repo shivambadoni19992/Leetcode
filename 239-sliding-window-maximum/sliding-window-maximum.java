511099c09a3a275087df8985e53bbaab71df24e7
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-05-03 15:23:50 +0530

    Time: 11 ms (97.53%) | Memory: 65 MB (6.28%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,28 +1,24 @@
 class Solution {
 
     public int[] maxSlidingWindow(int[] nums, int k) {
-        
         if (nums.length == 0 || k == 0) {
             return new int[0];
         }
         int n = nums.length;
-        int[] result = new int[n - k + 1]; 
+        int[] result = new int[n - k + 1];
 
-        
         int[] left = new int[n];
         int[] right = new int[n];
-        left[0] = nums[0]; 
+        left[0] = nums[0];
         right[n - 1] = nums[n - 1];
 
         for (int i = 1; i < n; ++i) {
-            
             if (i % k == 0) left[i] = nums[i]; else left[i] = Math.max(left[i - 1], nums[i]);
-            
+
             int j = n - i - 1;
             if (j % k == (k - 1)) right[j] = nums[j]; else right[j] = Math.max(right[j + 1], nums[j]);
         }
 
-        
         for (int i = 0, j = i + k - 1; j < n; ++i, ++j) {
             result[i] = Math.max(right[i], left[j]);
         }
