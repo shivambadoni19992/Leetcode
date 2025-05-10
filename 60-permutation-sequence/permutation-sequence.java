2490fc3a1d6d98275fa6787e637fa2a5c91427fd
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-05-10 11:10:53 +0530

    Time: 2 ms (48.42%) | Memory: 41.3 MB (41.08%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.java
+++ 60-permutation-sequence/permutation-sequence.java
@@ -1,31 +1,30 @@
 class Solution {
-
     List<Integer> list = new ArrayList<>();
 
-    public int fact(int n){
-        if(n == 0 || n == 1) return 1;
+    public int fact(int n) {
+        if (n == 0 || n == 1) return 1;
         return n * fact(n - 1);
     }
 
-    public String helper(String str, int n, int k){
+    public String helper(String str, int n, int k) {
         if (n == 0) {
             return str;
         }
 
         int f = fact(n - 1);
-        int group = (k - 1) / f;
-        k = (k - 1) % f + 1;
+        int group = k / f;
+        k = k % f + 1;
 
         str += list.get(group);
         list.remove(group);
 
-        return helper(str, n - 1, k);
+        return helper(str, n - 1, k - 1);
     }
 
     public String getPermutation(int n, int k) {
         for (int i = 1; i <= n; i++) {
             list.add(i);
         }
-        return helper("", n, k);
+        return helper("", n, k - 1);
     }
 }
