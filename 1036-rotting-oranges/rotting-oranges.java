986f7f8e5fe62a8820fc9f1e98a897cb03cc5300
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-05-31 00:30:56 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (29.89%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,41 +1,41 @@
 class Solution {
+
     public int orangesRotting(int[][] grid) {
         int count = 0;
-        int dir[][] =new int[][] { {0,1},{1,0} ,{-1,0} ,{0,-1} };
+        int dir[][] = new int[][] { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
         Queue<int[]> q = new LinkedList<>();
         int n = grid.length;
         int m = grid[0].length;
-        for(int i = 0;i < n;i++){
-            for(int j =0;j<m;j++) {
-                if(grid[i][j] == 2) {
-                    q.add(new int[]{i,j});
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new int[] { i, j });
                 }
             }
         }
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int size = q.size();
             count++;
-            for(int i =0;i<size;i++){
-                int []curr = q.poll();
-                for(int d[] : dir) {
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                for (int d[] : dir) {
                     int nr = d[0] + curr[0];
                     int nc = d[1] + curr[1];
-                    if(nr >= 0 && nr < n && nc >=0 && nc < m && grid[nr][nc] == 1) {
-                        q.add(new int[]{ nr,nc });
+                    if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1) {
+                        q.add(new int[] { nr, nc });
                         grid[nr][nc] = 2;
                     }
                 }
             }
         }
-        for(int i = 0;i < n;i++){
-            for(int j =0;j<m;j++) {
-                if(grid[i][j] == 1) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
                     return -1;
                 }
             }
         }
 
         return count == 0 ? 0 : count - 1;
-
     }
 }
