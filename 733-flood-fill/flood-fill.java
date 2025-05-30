5bdf69c6ce2215b3fdd2018fc7504c93fb03d85c
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-05-30 23:59:04 +0530

    Time: 1 ms (48.77%) | Memory: 45.3 MB (19.68%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,20 +1,21 @@
 class Solution {
+
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int currColor = image[sr][sc];
-        int dir[][] = new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
+        int dir[][] = new int[][] { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{sr,sc});
+        q.add(new int[] { sr, sc });
         image[sr][sc] = color;
         boolean vis[][] = new boolean[image.length][image[0].length];
         vis[sr][sc] = true;
 
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int pos[] = q.poll();
-            for(int d[] : dir) {
+            for (int d[] : dir) {
                 int row = pos[0] + d[0];
-                int column = pos[1]+d[1];
-                if(row >=0 && row < image.length && column>=0 && column < image[0].length && image[row][column] == currColor && !vis[row][column]) {
-                    q.add(new int[]{row,column});
+                int column = pos[1] + d[1];
+                if (row >= 0 && row < image.length && column >= 0 && column < image[0].length && image[row][column] == currColor && !vis[row][column]) {
+                    q.add(new int[] { row, column });
                     image[row][column] = color;
                     vis[row][column] = true;
                 }
