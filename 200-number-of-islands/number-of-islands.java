4e0b4bd9a50be3345f22aefa3bd7b7857eb8b648
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-05-31 00:16:18 +0530

    Time: 7 ms (18.56%) | Memory: 49.1 MB (90.89%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -8,21 +8,20 @@ class Solution {
         int[][] directions = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1' ) {
+                if (grid[i][j] == '1') {
                     q.add(new int[] { i, j });
+                    grid[i][j] = '0';
                     count++;
                     while (!q.isEmpty()) {
                         int a[] = q.poll();
                         int first = a[0];
                         int second = a[1];
-                        if(grid[first][second] == 0) continue;
-                        grid[first][second] = 0;
-                         for (int[] dir : directions) {
+                        for (int[] dir : directions) {
                             int newRow = first + dir[0];
                             int newCol = second + dir[1];
-                            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m
-                                    && grid[newRow][newCol] == '1') {
+                            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == '1') {
                                 q.add(new int[] { newRow, newCol });
+                                grid[newRow][newCol] = '0';
                             }
                         }
                     }
