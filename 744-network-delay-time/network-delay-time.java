e25f240e2eabd1a932790ca5ce3cc907713b7ad5
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-06-01 10:55:25 +0530

    Time: 20 ms (33.52%) | Memory: 48.4 MB (69.07%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,41 +1,39 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        Map<Integer, ArrayList<int[]>> adj = new HashMap<>();
+
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] time : times) {
+            graph.computeIfAbsent(time[0], x -> new ArrayList<>())
+                 .add(new int[]{time[1], time[2]});
+        }
+
         PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<>(){
             @Override
-            public int compare(int a[] ,int b[]){
+            public int compare(int []a,int []b) {
                 return a[1] - b[1];
             }
         });
-        for(int i =0;i< times.length;i++){
-            int src = times[i][0];
-            int dest = times[i][1];
-            int time = times[i][2];
-            if(!adj.containsKey(src)) {
-                adj.put(src,new ArrayList<>());
-            }
-            adj.get(src).add(new int[]{dest,time});
-        }
+        pq.offer(new int[]{k, 0});
+
         Set<Integer> visited = new HashSet<>();
-        pq.add(new int[]{k,0});
-        int minTime = Integer.MIN_VALUE;
-        while(!pq.isEmpty()){
-            int []curr = pq.poll();
-            int cs = curr[0];
-            int ct = curr[1];
-            if(!visited.contains(cs)) {
-                minTime = ct;
-                visited.add(cs);
-                if(adj.containsKey(cs)){
-                    ArrayList<int[]> destList = adj.get(cs);
-                    for(int i =0;i < destList.size();i++){
-                        pq.add(new int[]{destList.get(i)[0],ct + destList.get(i)[1]});
-                    }
+        int maxTime = 0;
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int node = curr[0], time = curr[1];
+
+            if (visited.contains(node)) continue;
+
+            visited.add(node);
+            maxTime = time;
+
+            for (int[] neighbor : graph.getOrDefault(node, new ArrayList<>())) {
+                if (!visited.contains(neighbor[0])) {
+                    pq.offer(new int[]{neighbor[0], time + neighbor[1]});
                 }
             }
         }
-        if(visited.size() != n) return -1;
-        return minTime;
 
+        return visited.size() == n ? maxTime : -1;
     }
 }
