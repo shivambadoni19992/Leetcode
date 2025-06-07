f0b8cebe8a234f6eff3aa4a30b41aa43d306a748
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-06-08 00:56:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (19.95%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,21 +1,21 @@
 class Solution {
-
-    public int search(int[] nums, int target) {
-        int start = 0, end = nums.length - 1;
-        while (start <= end) {
+    public int search(int[] arr, int target) {
+        int start = 0, end = arr.length -1;
+        while(start <= end) {
             int mid = start + (end - start) / 2;
-            System.out.println(mid);
-            if (target == nums[mid]) return mid;
-            if (nums[start] <= nums[mid]) {
-                if (nums[start] <= target && target<=nums[mid]) {
-                    end = mid - 1;
-                } else {
+            if(arr[mid] == target) {
+                return mid;
+            }
+            if(arr[start] <= arr[mid]) {
+                if(arr[start] <= target && target < arr[mid]) {
+                    end = mid -1;
+                } else{
                     start = mid + 1;
                 }
-            } else {
-                if (nums[mid] <= target && target <= nums[end]) {
-                    start = mid + 1;
-                } else {
+            } else{
+                if(arr[mid] < target && target <= arr[end]) {
+                    start = mid +1;
+                }else{
                     end = mid - 1;
                 }
             }
@@ -24,3 +24,13 @@ class Solution {
         return -1;
     }
 }
+
+//  4 5 6 7 0 1 2
+
+//  mid = 7
+//  4 < 7 ? left is sofrted 
+//  target between 4 and 7  then end mid -1
+//  else start = mid +1;
+//  else
+//  right is sorted 
+//  same as above
