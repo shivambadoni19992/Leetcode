8c31b8010915ff7c8fa061f84672a6d7a96b7a20
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-06-07 13:50:16 +0530

    Time: 4 ms (88.79%) | Memory: 58.4 MB (48.85%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,11 +1,21 @@
 class Solution {
-
     public int findDuplicate(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int num = Math.abs(nums[i]);
-            if (nums[num - 1] < 0) return num;
-            nums[num - 1] = nums[num - 1] * -1;
+        // iterate and change the sign of nums[nums[i] -1];
+
+        // if already negative return the number
+
+        for(int i = 0; i  < nums.length; i++){
+            int curr = Math.abs(nums[i]);
+            if(nums[curr] < 0) {
+                return curr;
+            }
+            nums[curr] = -nums[curr];
         }
         return -1;
     }
 }
+
+
+
+
+// arr = [-1 -3 -4 -2 2]  ans = 2
