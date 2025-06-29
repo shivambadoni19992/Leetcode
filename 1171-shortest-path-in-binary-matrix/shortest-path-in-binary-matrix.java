b8d62befd4629459f149c14ff1bec11b4da15770
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-06-29 10:05:28 +0530

    Time: 31 ms (16.30%) | Memory: 45.8 MB (31.96%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+
+        if(grid[0][0] == 1) return -1;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<>(){
+            @Override
+            public int compare(int a[],int b[]) {
+                return a[2] - b[2];
+            }
+        });
+        pq.offer(new int[]{0,0,1});
+        int directions[][] = new int[][]{ {0,1}, {1,0},{0,-1},{-1,0},{1,1},{-1,-1},{-1,1},{1,-1}};
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean vis[][] = new boolean[n][m];
+        vis[0][0] = true;
+        while(!pq.isEmpty()){
+            int []curr = pq.poll();
+            int currR = curr[0];
+            int currC = curr[1];
+            int currD = curr[2];
+            if(currR == n -1 && currC == m -1){
+                return currD;
+            }
+            for(int []dir : directions){
+                int newR = currR + dir[0];
+                int newC = currC + dir[1];
+                if(newR >=0 && newR < n && newC >=0 && newC < m && !vis[newR][newC] && grid[newR][newC] == 0){
+                    pq.offer(new int[]{newR, newC ,currD + 1});
+                    vis[newR][newC] = true;
+                }
+            }
+
+        }
+        return -1;
+    }
+}
