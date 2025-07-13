2e5b0cba0f59b826b0839bb8c02ef85cc95a6703
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-07-13 16:32:28 +0530

    Time: 414 ms (5.34%) | Memory: 79.8 MB (6.94%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,44 +1,23 @@
+import java.util.Arrays;
+
 class Solution {
-
-    // public int LIS(int[] nums, int ind, int ele) {
-    //     if (ind == 0) return (ele > nums[ind]) ? 1 : 0;
-    //     int res = -1;
-    //     int notTake = 0;
-    //     int take = 0;
-    //     if (nums[ind] >= ele) {
-    //         notTake = LIS(nums, ind - 1, ele);
-    //     } else {
-    //         take = 1 + LIS(nums, ind - 1, nums[ind]);
-    //         notTake = LIS(nums, ind - 1, ele);
-            
-    //     }
-    //     res = Math.max(take, notTake);
-    //     return res;
-    // }
-
     public int lengthOfLIS(int[] nums) {
-         int n=nums.length;
-        
-        int [] dp=new int[n];
-        
-        int lengthOfLIS=0;
-        
-        for(int current:nums){
-            
-            int i=0,j=lengthOfLIS;
-            
-            while(i!=j){
-                int mid=(i+j)/2;
-                
-                if(dp[mid]<current) i=mid+1;
-                
-                else j=mid;
-            }
-            
-            dp[i]=current;
-            
-            if(i==lengthOfLIS) lengthOfLIS++;
+        int n = nums.length;
+        int[][] dp = new int[n][n + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        return lengthOfLIS;
+        return f(0, -1, nums, n, dp);
+    }
+
+    private int f(int index, int prevInd, int[] nums, int n, int[][] dp) {
+        if (index == n) return 0;
+        if (dp[index][prevInd + 1] != -1) return dp[index][prevInd + 1];
+        int len = 0 + f(index + 1, prevInd, nums, n, dp);
+        if (prevInd == -1 || nums[index] > nums[prevInd]) {
+            len = Math.max(len, 1 + f(index + 1, index, nums, n, dp));
+        }
+        return dp[index][prevInd + 1] = len;
     }
 }
+
