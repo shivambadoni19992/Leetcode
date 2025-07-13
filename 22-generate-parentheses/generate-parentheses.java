4fe0fb1effc280c01f67250a10549cde18ac1ecc
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-07-13 18:44:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (94.54%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,27 +1,25 @@
 class Solution {
     List<String> ans = new ArrayList<>();
-
-    public void generate(StringBuilder s, int open, int close, int n) {
-        if (open == n && close == n) {
-            ans.add(s.toString());
+    public void generate(int n,int open, int close,StringBuilder sb){
+        if(n == close && n == open) {
+            ans.add(new String(sb));
             return;
         }
-        if (close < open) {
-            s.append(')');
-            generate(s, open, close + 1, n);
-            s.deleteCharAt(s.length() - 1);
+        if(close < open) {
+            sb.append(')');
+            generate(n, open, close +1 , sb);
+            sb.deleteCharAt(sb.length()-1);
         }
-        if (open < n) {
-            s.append('(');
-            generate(s, open + 1, close, n);
-            s.deleteCharAt(s.length() - 1);
+        if(open < n) {
+            sb.append('(');
+            generate(n, open + 1, close , sb);
+            sb.deleteCharAt(sb.length()-1);
+        }
 
-        }
         return;
     }
-
     public List<String> generateParenthesis(int n) {
-        generate(new StringBuilder(), 0, 0, n);
+        generate(n,0,0, new StringBuilder());
         return ans;
     }
 }
