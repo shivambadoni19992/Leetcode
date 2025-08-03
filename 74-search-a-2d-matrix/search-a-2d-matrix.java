601056649f8cdb822a4a1aedd7839def9e3aa6c8
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-08-03 10:48:41 +0530

    Time: 4 ms (2.21%) | Memory: 42.1 MB (81.38%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,20 +1,44 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int left = 0, right = m * n - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int mid_val = matrix[mid / n][mid % n];
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int start = 0, end = rows * columns - 1;
 
-            if (mid_val == target)
-                return true;
-            else if (mid_val < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
+        while(start <= end) {
+            int mid = (start + end) /2;
+            int row = mid / columns;
+            int column = mid % columns;
+            int midEle = matrix[row][column];
+            System.out.println(midEle);
+            if(midEle == target) return true;
+            if(midEle < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
         return false;
     }
 }
+
+// 1 2 3
+// 4 5 6
+// 7 8 9  search = 6
+// 8 / 2 = 4
+// start = 5 
+// end = 8 mid = 13/2 6
+
+// 1 2 3 4 5 6 7 8 9 
+
+// mid = 9 / 2 = 4 
+
+// row = 4 / 3 = 1
+// col = 4 %3 = 1
+// start = mid + 1
+// end = n-1
+
+// 1 3 5 7
+// 10 11 16 20
+// 23 30 34 60
+
