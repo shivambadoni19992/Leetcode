5b8c326fe1f2ff901c93412fccdb156810ec2b2f
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2025-09-30 14:34:56 +0530

    Time: 3 ms (95.11%) | Memory: 45.3 MB (31.36%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,28 +1,33 @@
 class Solution {
+    int helper(String word1, String word2, int idx1, int idx2, int [][]memo) {
+        if(idx1 == word1.length() && idx2 == word2.length()) return 0;
 
-     public int minDistance(String word1, String word2) {
-        int[][] memo = new int[word1.length()][word2.length()];
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
-        }
-        return minDist(0, 0, word1, word2, memo);
-    }
+        if(idx1 == word1.length()) return word2.length() - idx2 ;
+        if(idx2 == word2.length()) return word1.length()- idx1;
 
-    private int minDist(int i, int j, String word1, String word2, int[][] memo) {
-        if (i == word1.length()) return word2.length() - j;
-        if (j == word2.length()) return word1.length() - i;
-
-        if (memo[i][j] != -1) return memo[i][j];
-
-        if (word1.charAt(i) == word2.charAt(j)) {
-            memo[i][j] = minDist(i + 1, j + 1, word1, word2, memo);
+        if(memo[idx1][idx2] != -1) return memo[idx1][idx2];
+        int insert = Integer.MAX_VALUE;
+        int replace = Integer.MAX_VALUE;
+        int delete = Integer.MAX_VALUE;
+        if(word1.charAt(idx1) == word2.charAt(idx2)) {
+            return memo[idx1][idx2] = helper(word1, word2, idx1 + 1 , idx2 + 1, memo);
         } else {
-            int del = 1 + minDist(i + 1, j, word1, word2, memo);
-            int ins = 1 + minDist(i, j + 1, word1, word2, memo);
-            int rep = 1 + minDist(i + 1, j + 1, word1, word2, memo);
-            memo[i][j] = Math.min(del, Math.min(ins, rep));
+            insert = 1 + helper(word1, word2, idx1, idx2 + 1, memo);
+            delete = 1 + helper(word1, word2, idx1 + 1, idx2, memo);
+            replace = 1 + helper(word1, word2, idx1 + 1, idx2 + 1, memo);
+            return memo[idx1][idx2] = Math.min(insert, Math.min(delete, replace));
+
+        }
+    }
+    public int minDistance(String word1, String word2) {
+
+        int memo[][] = new int[word1.length()][word2.length()];
+
+        for(int i = 0; i< word1.length();i++) {
+            Arrays.fill(memo[i], -1);
         }
 
-        return memo[i][j];
+        return helper(word1, word2, 0, 0, memo);
+        
     }
 }
