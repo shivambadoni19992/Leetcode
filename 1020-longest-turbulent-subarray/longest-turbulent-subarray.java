527f984949420a3157c6fe142e7ac4a1b86969e8
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2026-01-14 22:43:52 +0530

    Time: 6 ms (85.20%) | Memory: 52 MB (12.64%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.java
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.java
@@ -1,19 +1,23 @@
 class Solution {
-    public int maxTurbulenceSize(int[] A) {
-        int N = A.length;
-        int ans = 1;
-        int anchor = 0;
+    public int maxTurbulenceSize(int[] nums) {
+        if(nums.length == 1) return nums.length;
+        if(nums.length == 2) return nums[0] == nums[1] ? 1 : 2;
+        int ans = 0;
+        int start = 0;
+        boolean greater = nums[0] > nums[1];
 
-        for (int i = 1; i < N; ++i) {
-            int c = Integer.compare(A[i-1], A[i]);
-            if (c == 0) {
-                anchor = i;
-            } else if (i == N-1 || c * Integer.compare(A[i], A[i+1]) != -1) {
-                ans = Math.max(ans, i - anchor + 1);
-                anchor = i;
-            }
+        for(int i = 2; i < nums.length; i++) {
+         boolean curr = nums[i - 1] > nums[i];
+         if(nums[i] == nums[i - 1]) {
+            start = i;
+         } else if (curr == greater) {
+            start = i -1;
+         } else {
+            
+            greater = curr;
+         }
+         ans = Math.max(ans, i - start + 1);
         }
-
         return ans;
     }
 }
