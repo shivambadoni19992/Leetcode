67264bc42940ac2991bb9c1a916c61bb3c972b1d
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2026-01-17 19:40:18 +0530

    Time: 150 ms (74.10%) | Memory: 47.2 MB (49.73%) - LeetSync

--- 212-word-search-ii/word-search-ii.java
+++ 212-word-search-ii/word-search-ii.java
@@ -1,59 +1,55 @@
-class Trie {
-    Trie letter[] = new Trie[26];
-    String word;
-}
-
 class Solution {
+    
 
-    Trie buildTrie(String words[]) {
-        Trie t = new Trie();
-
-        for(String word : words) {
-            Trie trie = t;
-            for(char ch : word.toCharArray()) {
-                if (trie.letter[ch - 'a'] == null) {
-                    trie.letter[ch - 'a'] = new Trie();
-                }
-                trie = trie.letter[ch - 'a'];
-            }
-            trie.word = word;
+        class Node {
+            Node letter[] = new Node[26];
+            String word;
         }
-        return t;
-    }
 
-    void dfs(char [][]board, Trie dict, int i, int j, List<String> result) {
-        
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] == '#' || dict.letter[board[i][j] - 'a'] == null) return;
+        Node trie = new Node();
 
-        char c = board[i][j];
-    Trie next = dict.letter[c - 'a'];
+        void insert(String word) {
+            Node itr = trie;
 
-    if (next == null) return;
-
-    if (next.word != null) {
-        result.add(next.word);
-        next.word = null;
-    }
-
-    board[i][j] = '#';
-
-    dfs(board, next, i + 1, j, result);
-    dfs(board, next, i - 1, j, result);
-    dfs(board, next, i, j + 1, result);
-    dfs(board, next, i, j - 1, result);
-
-    board[i][j] = c;
-
-
-    }
+            for(char ch : word.toCharArray()) {
+                int pos = ch - 'a';
+                if(itr.letter[pos] == null) {
+                    itr.letter[pos] = new Node();
+                }
+                itr = itr.letter[pos];
+            }
+            itr.word = word;
+        }
+    
     public List<String> findWords(char[][] board, String[] words) {
-        Trie trie = buildTrie(words);
-        List<String> result = new ArrayList<>();
+        Node itr = trie;
+        for(String word : words) {
+            insert(word);
+        }
+        List<String> ans = new ArrayList<>();
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[0].length; j++) {
-                dfs(board, trie, i, j, result);
+                dfs(board, i, j, itr, ans);
             }
         }
-        return result;
+        return ans;
+    }
+
+    void dfs(char [][]board, int row, int col, Node itr, List<String> ans) {
+        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] == '#' || itr.letter[board[row][col] - 'a'] == null) return;
+       
+
+        char ch = board[row][col];
+        board[row][col] = '#';
+        itr = itr.letter[ch - 'a'];
+        if(itr.word != null) {
+            ans.add(itr.word);
+            itr.word = null;
+        }
+        dfs(board, row + 1, col, itr, ans);
+        dfs(board, row, col + 1, itr, ans);
+        dfs(board, row - 1, col, itr, ans);
+        dfs(board, row, col - 1, itr, ans);
+        board[row][col] = ch;
     }
 }
