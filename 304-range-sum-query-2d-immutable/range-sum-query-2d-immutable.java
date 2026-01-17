00c39a82c87cba6152c2c76b2aa8c67db5543f79
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2026-01-17 12:42:32 +0530

    Time: 106 ms (44.65%) | Memory: 142 MB (53.80%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,22 +1,21 @@
 class NumMatrix {
-    int sum[][];
+    private final int[][] ps; // (n+1) x (m+1)
 
     public NumMatrix(int[][] matrix) {
-        sum = new int[matrix.length + 1][matrix[0].length + 1];
-        for (int i = 1; i <= matrix.length; i++) {
-            for (int j = 1; j <= matrix[0].length; j++) {
-                int curr = matrix[i - 1][j - 1];
-                sum[i][j] = curr + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+        int n = matrix.length;
+        int m = (n == 0) ? 0 : matrix[0].length;
+        ps = new int[n + 1][m + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ps[i+1][j+1] = matrix[i][j] + ps[i][j+1] + ps[i+1][j] - ps[i][j];
             }
         }
     }
 
-    public int sumRegion(int r1, int c1, int r2, int c2) {
-        return sum[r2 + 1][c2 + 1] - sum[r1][c2 + 1] - sum[r2 + 1][c1] + sum[r1][c1];
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return ps[row2 + 1][col2 + 1]
+             - ps[row1][col2 + 1]
+             - ps[row2 + 1][col1]
+             + ps[row1][col1];
     }
 }
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1,col1,row2,col2);
- */
