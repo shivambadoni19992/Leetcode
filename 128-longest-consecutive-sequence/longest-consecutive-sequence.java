4054fd0e1718d9e3e7978283e780662e1b6fcff6
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2026-01-18 13:26:16 +0530

    Time: 47 ms (11.29%) | Memory: 101.5 MB (5.01%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -16,40 +16,42 @@ class Solution {
             if(parent[u] == u) return u;
             return parent[u] = findPar(parent[u]);
         }
-        int unionBySize(int u, int v) {
+        void unionBySize(int u, int v) {
             int parU = findPar(u);
             int parV = findPar(v);
-            if(parU == parV) return size[parU];
+            if(parU == parV) return;
             if(size[parU] >= size[parV]) {
                 parent[parV] = parU;
                 size[parU] += size[parV];
-                return size[parU];
             } else {
                 parent[parU] = parV;
                 size[parV] += size[parU];
-                return size[parV];
             }
         }
-        
+        int findMax() {
+            int max = 0;
+            for(int i = 0; i < parent.length ; i++) {
+                max = Math.max(size[i], max);
+            }
+            return max;
+        }
     }
     public int longestConsecutive(int[] nums) {
         int n = nums.length;
-
-        if(n == 0) return 0;
         DisjointSet ds = new DisjointSet(n);
         Map<Integer, Integer> par = new HashMap<>();
-        int max = 1;
+
         for(int i = 0; i < n; i++) {
             int num = nums[i];
             if(par.containsKey(num)) continue;
             if(par.containsKey(num - 1)) {
-                max = Math.max(max, ds.unionBySize(par.get(num - 1), i));
+                ds.unionBySize(par.get(num - 1), i);
             }
             if(par.containsKey(num + 1)) {
-                max = Math.max(max, ds.unionBySize(par.get(num + 1), i));
+                ds.unionBySize(par.get(num + 1), i);
             }
             par.put(num, i);
         }
-        return max;
+        return ds.findMax();
     }
 }
