c32d3a1eeca513becb400d92f241b5fbd22f7b51
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2026-01-19 19:04:23 +0530

    Time: 27 ms (24.40%) | Memory: 54.2 MB (15.10%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,29 +1,26 @@
 class Solution {
 
-    // public int findlongest(String s1, String s2, int i, int j) {
-    //     if (i == s1.length() || j == s2.length()) return 0;
-    //     int res = 0;
-    //     if (s1.charAt(i) == s2.charAt(j)) res = 1 + findlongest(s1, s2, i + 1, j + 1);
-    //     int left = findlongest(s1, s2, i + 1, j);
-    //     int right = findlongest(s1, s2, i, j + 1);
-    //     return Math.max(res, Math.max(left, right));
-    // }
-
+    int helper(int ind1, String text1, int ind2, String text2, int dp[][]) {
+        if(ind1 == text1.length() || ind2 == text2.length()) {
+            return 0;
+        }
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        if(text1.charAt(ind1) == text2.charAt(ind2)) {
+            return dp[ind1][ind2] = 1 + helper(ind1 + 1, text1, ind2 + 1, text2, dp);
+        } else {
+            return dp[ind1][ind2] = Math.max(helper(ind1 + 1, text1, ind2, text2, dp), helper(ind1, text1, ind2 + 1, text2, dp));
+        }
+    }
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
-        int n2 = text2.length();
-        int dp[][] = new int[n + 1][n2 + 1];
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n2 - 1; j >= 0; j--) {
-                if (text1.charAt(i) == text2.charAt(j)) {
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
-                } else {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
-                }
-                // System.out.println(dp[i][j]);
-            }
+        int m = text2.length();
+
+        int dp[][] = new int[n][m];
+
+        for(int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        return dp[0][0];
-        // return findlongest(text1,text2,0,0);
+
+        return helper(0, text1, 0, text2, dp);
     }
 }
