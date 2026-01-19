59596ec7fd63288fd33f799be6b74a2307fbbebc
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2026-01-19 14:38:54 +0530

    Time: 36 ms (12.86%) | Memory: 47.4 MB (13.18%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,25 +1,25 @@
 class Solution {
-    public int helper(int ind, int[] coins, int amount, int[][] memo) {
-        if (amount == 0) return 0;
-        if (ind == coins.length || amount < 0) return Integer.MAX_VALUE - 1;
 
-        if (memo[ind][amount] != -1) return memo[ind][amount];
+    int helper(int ind, int coins[], int amount, int dp[][]) {
 
-        int take = Integer.MAX_VALUE - 1;
-        if (coins[ind] <= amount) {
-            take = 1 + helper(ind, coins, amount - coins[ind], memo);
-        }
-        int notTake = helper(ind + 1, coins, amount, memo);
+        if(amount == 0 && ind == coins.length) return 0;
+        if(amount < 0 || ind == coins.length) return Integer.MAX_VALUE - 1;
 
-        return memo[ind][amount] = Math.min(take, notTake);
+        if(dp[ind][amount] != -1) return dp[ind][amount];
+        int take = 1 + helper(ind, coins, amount - coins[ind], dp);
+        int notTake =  helper(ind + 1, coins, amount, dp);
+        return dp[ind][amount] = Math.min(take, notTake); 
+
     }
-
     public int coinChange(int[] coins, int amount) {
-        int[][] memo = new int[coins.length][amount + 1];
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
+        int n = coins.length;
+        int dp[][] = new int[n][amount + 1];
+
+        for(int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        int result = helper(0, coins, amount, memo);
-        return result == Integer.MAX_VALUE - 1 ? -1 : result;
+        int totalCoins =  helper(0, coins, amount, dp);
+        return totalCoins == Integer.MAX_VALUE - 1 ? -1 : totalCoins;
+        
     }
 }
