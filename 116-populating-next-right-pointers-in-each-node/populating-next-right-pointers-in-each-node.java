28d767a4ed4ec54cdb1f25fec02a562ffe67f59e
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2026-02-01 04:56:58 +0530

    Time: 2 ms (52.73%) | Memory: 46.5 MB (48.04%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -1,46 +1,22 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
-
     public Node connect(Node root) {
-        Node head = root;
         if (root == null) return null;
-        Queue<Node> q = new LinkedList<>();
-        q.add(head);
-        while (!q.isEmpty()) {
-            int n = q.size();
-            Node prev = null;
-            for (int i = 0; i < n; i++) {
-                Node a = q.poll();
-                if (prev != null) {
-                    prev.next = a;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node rightNode = null;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node curr = queue.poll();
+                curr.next = rightNode;
+                rightNode = curr;
+                if (curr.right != null) {
+                    queue.add(curr.right);
                 }
-                prev = a;
-                if (a.left != null) q.add(a.left);
-                if (a.right != null) q.add(a.right);
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
             }
-            prev.next = null;
         }
         return root;
     }
