07d4fb71416fe2b8877e5834828c8415800199a5
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2026-02-13 06:52:02 +0530

    Time: 1 ms (87.82%) | Memory: 44.2 MB (56.51%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,18 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-
-    public void find(int start, int nums[], List<Integer> list) {
-        ans.add(new ArrayList(list));
-        for (int i = start; i < nums.length; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) continue;
-            list.add(nums[i]);
-            find(i + 1, nums, list);
-            list.remove(list.size() - 1);
+    void helper(int []nums, int start, List<Integer> list, List<List<Integer>> ans) {
+        if(start == nums.length) {
+            ans.add(new ArrayList<>(list));
+            return;
         }
-    }
 
+        helper(nums, start + 1, list, ans);
+        list.add(nums[start]);
+        helper(nums, start + 1, list, ans);
+        list.remove(list.size() - 1);
+    }
     public List<List<Integer>> subsets(int[] nums) {
-        Arrays.sort(nums);
-        find(0, nums, new ArrayList<>());
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(nums, 0, new ArrayList<>(), ans);
         return ans;
     }
 }
