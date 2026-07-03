22529b8b59c29d57826f93174d0cd7dbfe5093c0
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2026-07-03 11:21:43 +0530

    Time: 30 ms (76.75%) | Memory: 95.8 MB (27.96%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,73 +1,27 @@
 class Solution {
-
-    class DisjointSet {
-        Map<Integer, Integer> comp = new HashMap<>();
-
-        Map<Integer, Integer> par = new HashMap<>();
-
-        public DisjointSet() {
-
-        }
-
-        void setPar(int u) {
-            par.put(u, u);
-        }
-
-
-        boolean contains(int u) {
-            return par.containsKey(u);
-        }
-
-        int findPar(int u) {
-            if(par.get(u) == u) return u;
-            par.put(u, findPar(par.get(u)));
-            return par.get(u);
-        }
-
-        void union(int u, int v) {
-            int parU = findPar(u);
-            int parV = findPar(v);
-
-            if(parU == parV) return;
-
-            int sizeU = comp.getOrDefault(parU, 1);
-            int sizeV = comp.getOrDefault(parV, 1);
-            int newSize = sizeU + sizeV;
-            if(sizeU <= sizeV) {
-                par.put(parU, parV);
-                comp.put(parV, newSize);
-            } else {
-                par.put(parV, parU);
-                comp.put(parU, newSize);
-            }
-        }
-
-        int findMax() {
-            int max = 0;
-            for(int key : par.keySet()) {
-                if(par.get(key) == key) {
-                    max = Math.max(max, comp.getOrDefault(key, 1));
-                }
-            }
-            return max;
-        }
-    }
     public int longestConsecutive(int[] nums) {
-        DisjointSet dsu = new DisjointSet();
+        Set<Integer> set = new HashSet<>();
 
         for(int num : nums) {
-            if(dsu.contains(num)) continue;
+            set.add(num);
+        }
 
-            dsu.setPar(num);
+        int max = 0;
 
-            if(dsu.contains(num - 1)) {
-                dsu.union(num, num - 1);
-            }
+        for(int num : set) {
+            if(!set.contains(num - 1)) {   // start of sequence
+                int curr = num;
+                int len = 1;
 
-            if(dsu.contains(num + 1)) {
-                dsu.union(num, num + 1);
+                while(set.contains(curr + 1)) {
+                    curr++;
+                    len++;
+                }
+
+                max = Math.max(max, len);
             }
         }
-        return dsu.findMax();
+
+        return max;
     }
 }
