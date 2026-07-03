4bcbeaa3ec15f9762644c12b8b03399bf5bdaf4f
shivambadoni19992 <42020140+shivambadoni19992@users.noreply.github.com>
2026-07-03 07:27:44 +0530

    Time: 46 ms (18.33%) | Memory: 59.1 MB (73.93%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,34 +1,48 @@
 class Solution {
-
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
+
+        List<List<Integer>> res = new ArrayList<>();
+        
+        int n = nums.length;
+        
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length ; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            int j = i + 1, k = nums.length - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == 0) {
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[j]);
-                    temp.add(nums[k]);
-                    ans.add(temp);
-                    while (j < k && nums[j] == nums[j + 1]) {
+
+        for(int i = 0; i < n; i++) {
+
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1, k = n - 1;
+            int target = -1 * nums[i];
+            while(j < k) {
+
+                if(j > i + 1 && nums[j] == nums[j - 1]) {
+                    j++;
+                    
+                } else if(k < n - 1 && nums[k] == nums[k + 1]) {
+                    k--;
+                    
+                } else {
+                    
+                    int sum = nums[j] + nums[k];
+
+                    if(sum == target) {
+                        res.add(List.of(nums[i], nums[j], nums[k]));
                         j++;
-                    }
-                    while (k > j && nums[k] == nums[k - 1]) {
+                        k--;
+                    } else if (sum < target) {
+                        j++;
+                    } else {
                         k--;
                     }
-                    j++;
-                    k--;
-                } else if (sum > 0) k--; else j++;
+
+                }
             }
+
+           
+
+
         }
+         return res;
         
-        
-        return ans;
     }
 }
